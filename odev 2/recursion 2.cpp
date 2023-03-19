a33dae2db8e0b0907847d0eb21c3f8f65bafd5a8
yarenyazar <yarenyazar67@gmail.com>
2023-03-19 22:54:51 +0300

    fix:week3

--- odev 2/recursion 2.cpp
+++ odev 2/recursion 2.cpp
@@ -1,18 +1,5 @@
 #include<stdio.h>
 
-int noOfDigits(int);
-int main(){
-	
-	int number, i;
-		printf("enter a number:");
-		scanf("%d",&number);
-		
-		i= countDigits(number));
-		
-		printf("the number of digits in the number: %d",i);
-		return 0;
-}
-
 int countDigits(int num){
 	
 	static int ctr=0;
@@ -24,3 +11,17 @@ int countDigits(int num){
 	
 	return ctr;
 }
+
+int main(){
+	
+	int number, i;
+		printf("enter a number:");
+		scanf("%d",&number);
+		
+		i= countDigits(number);
+		
+		printf("the number of digits in the number: %d",i);
+		return 0;
+}
+
+
